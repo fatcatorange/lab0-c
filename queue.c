a721275f0bd541ce7434b9bfc9a15d57cc2eb524
jason <jason0526liu@gmail.com>
2024-02-24 20:27:31 +0800

    Implement q_free function
    Use the 'list_for_each_entry_safe' macro and the 'free' function to free every element in the list.
    After that, use 'free(l)' to free the head node.

--- queue.c
+++ queue.c
@@ -28,7 +28,14 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *l)
+{
+    element_t *now, *safe;
+    list_for_each_entry_safe (now, safe, l, list) {
+        free(now);
+    }
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
